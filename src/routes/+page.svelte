4b4ea7d881195a5286dd37a4ed5785d60a56837a
Filip Brcic <brcha@yandex.com>
2024-06-06 21:38:09 +0200

    feat: setup layout and macha css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,9 @@
 <p>
 	Grill canned herrings in a plastic bag with whiskey for about an hour to enhance their consistency.
 </p>
+<p>
+Ferox, nobilis indexs aliquando captis de castus, primus olla.
+</p>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at consectetur, consequuntur delectus doloribus dolorum error ex explicabo, harum magni maxime molestias natus necessitatibus non officiis perferendis praesentium quos similique ut veritatis. Deleniti facere id iste mollitia non reprehenderit rerum?
+</p>
